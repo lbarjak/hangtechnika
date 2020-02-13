7569224f55059e55119bc1fb662dd74a1b6684dc
lbarjak <barjak.laszlo@gmail.com>
2020-02-13 22:26:45 +0100

    files sort by creation date

--- src/main/java/hangtechnika/Tools.java
+++ src/main/java/hangtechnika/Tools.java
@@ -4,11 +4,14 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Locale;
@@ -18,7 +21,7 @@ import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.openxml4j.exceptions.OpenXML4JException;
 
 public class Tools implements GlobalVariables {
-	
+
 	Set<String> htKeys;
 
 	public String now() {
@@ -68,22 +71,23 @@ public class Tools implements GlobalVariables {
 		new FromXLSX().read(xlsxName, HANGZAVAR_MAP);
 		htKeys = HANGZAVAR_MAP.get("export").keySet();
 	}
-	
+
 	public String round(String numberSring) {
 		Integer num = (int) Math.round(Double.parseDouble(numberSring));
 		return num.toString();
 	}
+
 	public String round(Double number) {
 		Integer num = (int) Math.round(number);
 		return num.toString();
 	}
-	
+
 	public String round2(String numberSring) {
 		Double num = Math.round(100.0 * (Double.parseDouble(numberSring))) / 100.0;
 		DecimalFormat df = new DecimalFormat("0,00");
 		return df.format(num);
 	}
-	
+
 	public String decimalFormat(double number) {
 		Locale locale = new Locale("hu", "HU");
 		String pattern = "0.00";
@@ -92,20 +96,44 @@ public class Tools implements GlobalVariables {
 		String formatted = decimalFormat.format(number);
 		return formatted;
 	}
-	
+
 	String fileName(String regex) {
 		File folder = new File("../hangtechnika_files");
+		ArrayList<File> listOfXLSX = new ArrayList<>();
 		if (folder.exists()) {
 			File[] listOfFiles = folder.listFiles();
 			for (File file : listOfFiles) {
 				if (file.isFile() && file.getName().matches(regex)) {
-					return folder + "/" + file.getName();
+					listOfXLSX.add((File) file);
 				}
 			}
+			File[] listOfXLSXToSort = new File[listOfXLSX.size()];
+			listOfXLSXToSort = listOfXLSX.toArray(listOfXLSXToSort);
+			sortFilesByDateCreated(listOfXLSXToSort);
+			return folder + "/" + listOfXLSXToSort[0].getName();
 		} else {
 			System.out.println("A folder nem létezik!");
 		}
 		return "";
+	}
+
+	public void sortFilesByDateCreated(File[] files) {
+		Arrays.sort(files, new Comparator<File>() {
+			public int compare(File f1, File f2) {
+				long l1 = getFileCreationEpoch(f1);
+				long l2 = getFileCreationEpoch(f2);
+				return Long.valueOf(l1).compareTo(l2);
+			}
+		});
+	}
+
+	public long getFileCreationEpoch(File file) {
+		try {
+			BasicFileAttributes attr = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
+			return attr.creationTime().toInstant().toEpochMilli();
+		} catch (IOException e) {
+			throw new RuntimeException(file.getAbsolutePath(), e);
+		}
 	}
 
 }
